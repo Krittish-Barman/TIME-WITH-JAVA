9c57fe618f385e7979fa45c219a702ccd96c225b
Krittish Barman <163887749+Krittish-Barman@users.noreply.github.com>
2026-03-26 10:04:51 +0530

    better

--- intro.java
+++ intro.java
@@ -1,7 +1,15 @@
-class firstclass {
+import java.util.*;
+
+public class firstclass {
     public static void main(String[] args) {
         System.out.println("*");
         System.out.println("**");
+        String name = "Hello World";
+        int a = 10;
+        System.out.println(name);
 
+        Scanner sc = new Scanner(System.in);
+        String nam = sc.nextLine();
+        System.out.println(nam);
     }
 }
