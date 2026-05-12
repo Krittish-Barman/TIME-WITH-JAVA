aaec1975bfdf24011afacd7ebc7f356d5392b170
Krittish Barman <163887749+Krittish-Barman@users.noreply.github.com>
2026-05-12 06:38:46 +0530

    Add files via upload
    some more concepts in java

---
+++ whi_loop.java
@@ -0,0 +1,25 @@
+class whi_loop{
+public static void main(String[] args){
+int i = 0;
+while(i < 11){
+System.out.println(i);
+i = i+1;
+}
+}
+}
+
+
+// do while
+
+class do_whi{
+public static void main(String[] args){
+int i = 14;
+do{
+System.out.println("hi");
+
+} while(i<11);
+}
+}
+
+
+
